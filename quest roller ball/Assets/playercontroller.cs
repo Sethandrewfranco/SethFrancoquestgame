12ff5cd98166a7ff55ce0e495e15e7a8117c42e4
Sethandrewfranco <43960700+Sethandrewfranco@users.noreply.github.com>
2018-11-04 15:38:06 -0500

    game

---
+++ quest roller ball/Assets/playercontroller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playercontroller : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody rb;
+    public Text winText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        winText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * -speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Quest"))
+        {
+            other.gameObject.SetActive(false);
+
+            winText.text = "You have hit the back wall, jumped the ramp, hit the stop wall, and landed on the lower level";
+        }
+        if (other.gameObject.CompareTag("Object"))
+        {
+            other.gameObject.SetActive(false);
+
+        }
+    }
+}
